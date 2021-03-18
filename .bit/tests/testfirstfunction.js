bfd0d7bb1325c3086a293257953d8d2a6614cccd
emsesc <emseschen@gmail.com>
2021-03-17 22:55:52 -0400

    Very first function test complete
    test file to test HTTP trigger function

--- .bit/tests/testfirstfunction.js
+++ .bit/tests/testfirstfunction.js
@@ -2,17 +2,22 @@ let uri = undefined
 const fetch = require('node-fetch');
 
 uri = process.env.HTTP_ENDPOINT
+
 if (uri == null) {
     throw new Error("You have not added your function url as a secret!");
 }
 
-
-try {
-    fetch(uri)
-        .then(response => console.log("Success 🎉! We got: " + response.json()))
-} catch (e) {
-    throw new Error("Try again, we didn't get a response.")
-}
-
+(async () => {
+    const resp = await fetch(uri, {
+        method: 'GET'
+    });
+    var data = await resp.text()
+    let test = JSON.stringify(data)
+    if (test.length < 3) {
+        console.log("No response... Try again!")
+    } else {
+        console.log("Yay! 🎉 We got: " + JSON.stringify(data))
+    }
+})();
 
 
