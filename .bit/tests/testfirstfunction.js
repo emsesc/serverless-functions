2ef2358821feef42b92a2ef2700cc822ef3e4f16
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-03-17 23:08:17 -0400

    Update testfirstfunction.js

--- .bit/tests/testfirstfunction.js
+++ .bit/tests/testfirstfunction.js
@@ -7,15 +7,19 @@ if (uri == null) {
     throw new Error("You have not added your function url as a secret!");
 }
 
-(async () => {
+async function testRequest(uri) {
     const resp = await fetch(uri, {
         method: 'GET'
     });
     var data = await resp.text()
     let test = JSON.stringify(data)
-    
+    return test
+}
+
+(async () => {
+    let test = await testRequest(uri)
     if (test.length < 3) {
-        Promise.reject(new Error("No response... Try again!"))
+        throw new Error("No response... Try again!"))
     } else {
         console.log("Yay! 🎉 We got: " + JSON.stringify(data))
     }
