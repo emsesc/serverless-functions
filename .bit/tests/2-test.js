bc1863db4a32ab98dc9c1e28993fcb2b8ca9035c
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-03-19 14:05:31 -0400

    Update 2-test.js

--- .bit/tests/2-test.js
+++ .bit/tests/2-test.js
@@ -12,7 +12,8 @@ if (uri == null) {
 (async () => {
     fs.readFile(`${__dirname}/testimage.jpg`, async function(err, content) {
         let formData = new FormData()
-        formData.append('file', fs.readFileSync(`${__dirname}/testimage.jpg`), {data: content})
+        formData.append('file', {data: content})
+        console.log(formData)
         const resp = await fetch(uri, {
             method: 'POST',
             body: formData,
