53540a3a4e434ac2192179bc34d3b9e18f6ef621
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-03-19 11:55:36 -0400

    Update 2-test.js

--- .bit/tests/2-test.js
+++ .bit/tests/2-test.js
@@ -1,23 +1,27 @@
 let uri = undefined
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-uri = process.env.HTTP_ENDPOINT
+uri = process.env.FUNCTION_URL
 
 if (uri == null) {
     throw new Error("You have not added your function url as a secret!");
 }
 
 (async () => {
-    const resp = await fetch(uri, {
-        method: 'GET'
-    });
-    var data = await resp.text()
-    let test = JSON.stringify(data)
-    
-    if (test.length < 3) {
-        console.log("No response... Try again!")
-        process.exit(1)
-    } else {
-        console.log("Yay! 🎉 We got: " + JSON.stringify(data))
+    fs.readFile('./testimage.jpg', function(err, data) {
+        const resp = await fetch(uri, {
+            method: 'POST',
+            body: data
+        });
+        var result = await resp.text()
+        let test = JSON.stringify(result)
+
+        if (test.length < 3) {
+            console.log("No response... Try again!")
+            process.exit(1)
+        } else {
+            console.log("Yay! 🎉 We got: " + test)
     }
+  })
 })();
