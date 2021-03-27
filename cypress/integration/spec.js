1a8632a192e3b4813741a137e065cd04a16455e2
emsesc <emseschen@gmail.com>
2021-03-26 20:55:57 -0400

    Update spec.js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
-describe('My First Test', () => {
-  it('finds the content "emotion"', () => {
+describe('Increment Count', () => {
+  it('Clicks button to increment', () => {
     cy.visit('./index.html')
-    cy.contains('emotion')
+    cy.get('[id=counter]').click()
+    cy.get('span').should(($span) => {
+      const text = $span.text()
+    
+      expect(text).to.include('2')
+    })
   })
 })
